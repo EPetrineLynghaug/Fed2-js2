17cf184d1079869196505c94b0d06e6fa107466e
EPetrineLynghaug <136695793+EPetrineLynghaug@users.noreply.github.com>
2024-09-26 18:57:14 +0200

    .

--- src/app.js
+++ src/app.js
@@ -1,17 +1,4 @@
-// @ts-check
-// INFO: README:
-// Adding @ts-check at the top of a file
-// will add type checking with JSDoc.
-// Read more about JSDoc:
-//
-// https://jsdoc.app/
-// https://www.npmjs.com/package/jsdoc
-
 import "./css/style.css";
-import navbar from "./js/components/navbar";
-
 import router from "./js/router";
-
-navbar();
 
 await router(window.location.pathname);
--- src/js/router/views/home.js
+++ src/js/router/views/home.js
@@ -1,9 +1,11 @@
-import { readPost, readPosts } from "../../api/post/read";
+import { readPosts } from "../../api/post/read";
 import { authGuard } from "../../utilities/authGuard";
 import { getUserInfo } from "../../utilities/userInfo";
 import createPostCards from "../../utilities/post-card";
+import navbar from "../../components/navbar";
 
 authGuard();
+navbar();
 
 const userInfo = getUserInfo();
 const name = userInfo.name;
